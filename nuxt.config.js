01640e6de9245289651a23dfb9787579443e048b
harukifumino <fu.miharun11@gmail.com>
2020-07-25 03:59:52 +0900

    add ogp settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,10 +22,19 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'og:url', property: 'og:url', content: 'https://miharun.netlify.app'},
+      { hid: 'og:type', property: 'og:type', content: 'website'}, // TODO: use 'article' for all of our pages except for the top page.
+      { hid: 'og:title', property: 'og:title', content: 'Miharun\'s Portfolio'},
+      { hid: 'og:description', property: 'og:description', content: 'Welcome to miharun\'s portfolio.'},
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Miharun\'s Portfolio'},
+      { hid: 'og:image', property: 'og:image', content: 'https://raw.githubusercontent.com/harukifumino/portfolio/master/og_image.png'},
+      { hid: 'og:image:width', property: 'og:image:width', content: '1200'},
+      { hid: 'og:image:height', property: 'og:image:height', content: '630'},
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'manifest', href: '/manifest.json' },
     ]
   },
   /*
@@ -50,6 +59,7 @@ export default {
   buildModules: [
     '@nuxt/typescript-build',
     '@nuxtjs/vuetify',
+    '@nuxtjs/pwa',
   ],
   /*
   ** Nuxt.js modules
