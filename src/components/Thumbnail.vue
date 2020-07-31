a4a05ae1ed7f2fb4abd85e58e3cac61ff10b5b9c
harukifumino <fu.miharun11@gmail.com>
2020-07-31 16:59:17 +0900

    update index

---
+++ src/components/Thumbnail.vue
@@ -0,0 +1,15 @@
+<template>
+  <img
+    class="profile__avatar"
+    src="https://avatars2.githubusercontent.com/u/28529274?s=460&u=42f0705f0dc8ff790205021353a047fc04f52adb&v=4"
+  />
+</template>
+
+<style>
+.profile__avatar {
+  height: 192px;
+  width: 192px;
+  border-radius: 50%;
+  filter: drop-shadow(4px 4px 8px #696969);
+}
+</style>
